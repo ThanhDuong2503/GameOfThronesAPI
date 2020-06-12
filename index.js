ddff8e4c96c4573ab2842cf53b18b3bb1521902e
Thanh <Thanh.Duong2503@gmx.de>
2020-06-12 17:00:49 +0200

    AddButtons

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+const listMalesButton = document.getElementById("maleChars");
+listMalesButton.addEventListener("click", ()=> listMaleChars());
+
+const listFemalesButton = document.getElementById("femaleChars");
+listFemalesButton.addEventListener("click", ()=> listFemaleChars());
+
+const listNamesButton = document.getElementById("charsName");
+listNamesButton.addEventListener("click", ()=> listNames());
 
 async function getData() {
 
@@ -11,35 +19,42 @@ async function getData() {
         return charDb;
 }
 
+// erzeugt neues Array mit weiblichen / männlichen Charaktere
 function filterCharactersByGender(allChars, gender) {
     console.log(allChars.filter(char => char.gender===gender));
 }
 
+function listMaleChars(){getData().then(data => {
+    filterCharactersByGender(data, "Male")
+})};
+
+function listFemaleChars(){getData().then(data => {
+    filterCharactersByGender(data, "Female")
+})};
+
+// erzeugt neues Array nur mit Namen der Charaktere
 function mapCharactersByAlias(allChars) {
     console.log(allChars.map(char => char.aliases))
 }
-
-getData().then(data => {
-    filterCharactersByGender(data, "Male")
-    filterCharactersByGender(data, "Female")
+function listNames(){getData().then(data => {
     mapCharactersByAlias(data)
-})
+})};
 
 
 
-function renderToDom(targetElementId, createElementType, innerHTMLString) {
-
-    // gets HTML element with specified id from DOM
-    const targetElement = document.getElementById(targetElementId);
-
+function renderToDom(targetElementId, createElementType, addCreateElementClass, innerHTMLString) {
     // creates an HTML element of selected type
     // should be a string.
     // example: "li" creates a <li> element
     const element = document.createElement(createElementType);
-
+    // gets HTML element with specified id from DOM
+    const targetElement = document.getElementById(targetElementId);
     // appends previously created element
     // to element with specified id
     targetElement.appendChild(element);
+    // add specified class to previously
+    // created element
+    targetElement.classList.add(addCreateElementClass);
     // sets inner HTML of previously created element
     // to provided string. This can be valid HTML, just
     // some data of type String or undefined
@@ -48,4 +63,22 @@ function renderToDom(targetElementId, createElementType, innerHTMLString) {
     return element;
 }
 
-renderToDom("charGoTDb", "string" , getData())
+renderToDom("charGoTDb", "div" , getData());
+
+    //fetch --> array als rückgabe
+// fetchPeople().then((people) =>
+//     people.map((person) => {
+//         renderToDom(
+//             "listOfPeople",
+//             "div",
+//             `${person.first_name}, ${person.last_name}`
+//         );
+//     })
+// );
+
+// async function fetchPeople() {
+//     const people = fetch("https://reqres.in/api/users")
+//         .then((response) => response.json())
+//         .then((data) => data.data);
+//     return people;
+// }
