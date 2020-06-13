3997164becaf97b80f87631b3b04113a1e5ac474
Thanh <Thanh.Duong2503@gmx.de>
2020-06-13 20:40:47 +0200

    AddRender

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const listMalesButton = document.getElementById("maleChars");
-listMalesButton.addEventListener("click", ()=> listMaleChars());
+listMalesButton.addEventListener("click", ()=> renderMaleCharacters());
 
 const listFemalesButton = document.getElementById("femaleChars");
 listFemalesButton.addEventListener("click", ()=> listFemaleChars());
@@ -40,44 +40,56 @@ function listNames(){getData().then(data => {
     mapCharactersByAlias(data)
 })};
 
-
-function renderToDom(targetElementId, createElementType, addCreateElementClass, innerHTMLString) {
-    // creates an HTML element of selected type
-    // should be a string.
-    // example: "li" creates a <li> element
-    const element = document.createElement(createElementType);
-    // gets HTML element with specified id from DOM
-    const targetElement = document.getElementById(targetElementId);
-    // appends previously created element
-    // to element with specified id
-    targetElement.appendChild(element);
-    // add specified class to previously
-    // created element
-    targetElement.classList.add(addCreateElementClass);
-    // sets inner HTML of previously created element
-    // to provided string. This can be valid HTML, just
-    // some data of type String or undefined
-    innerHTMLString ? (element.innerHTML = innerHTMLString) : "";
-    // returns element so the element can be manipulated
-    return element;
+function renderMaleCharacters() {
+    const listOfResult = document.getElementById("renderResult");
+    let resultList = listMaleChars();
+    for (let result of Object.values(resultList)) {
+        let newListItem = document.createElement("li");
+        newListItem.innerHTML = result;
+        listOfResult.appendChild(newListItem);
+    }
 }
 
-// renderToDom("charGoTDb", "div" , "name", getData());
 
-    //fetch --> array als rückgabe
-// fetchPeople().then((people) =>
-//     people.map((person) => {
-//         renderToDom(
-//             "listOfPeople",
-//             "div",
-//             `${person.first_name}, ${person.last_name}`
-//         );
-//     })
-// );
 
-// async function fetchPeople() {
-//     const people = fetch("https://reqres.in/api/users")
-//         .then((response) => response.json())
-//         .then((data) => data.data);
-//     return people;
+
+// function renderToDom(targetElementId, createElementType, addCreateElementClass, innerHTMLString) {
+//     // creates an HTML element of selected type
+//     // should be a string.
+//     // example: "li" creates a <li> element
+//     const element = document.createElement(createElementType);
+//     // gets HTML element with specified id from DOM
+//     const targetElement = document.getElementById(targetElementId);
+//     // appends previously created element
+//     // to element with specified id
+//     targetElement.appendChild(element);
+//     // add specified class to previously
+//     // created element
+//     targetElement.classList.add(addCreateElementClass);
+//     // sets inner HTML of previously created element
+//     // to provided string. This can be valid HTML, just
+//     // some data of type String or undefined
+//     innerHTMLString ? (element.innerHTML = innerHTMLString) : "";
+//     // returns element so the element can be manipulated
+//     return element;
 // }
+//
+// // renderToDom("charGoTDb", "div" , "name", getData());
+//
+//     //fetch --> array als rückgabe
+// // fetchPeople().then((people) =>
+// //     people.map((person) => {
+// //         renderToDom(
+// //             "listOfPeople",
+// //             "div",
+// //             `${person.first_name}, ${person.last_name}`
+// //         );
+// //     })
+// // );
+//
+// // async function fetchPeople() {
+// //     const people = fetch("https://reqres.in/api/users")
+// //         .then((response) => response.json())
+// //         .then((data) => data.data);
+// //     return people;
+// // }
