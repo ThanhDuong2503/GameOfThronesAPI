f286e7e31470fcf231ef14352e6e01dc3a9ccd6b
Thanh <Thanh.Duong2503@gmx.de>
2020-06-14 11:57:13 +0200

    AddSomeTestExercises

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const listMalesButton = document.getElementById("maleChars");
-listMalesButton.addEventListener("click", ()=> renderMaleCharacters());
+listMalesButton.addEventListener("click", ()=> listMaleChars());
 
 const listFemalesButton = document.getElementById("femaleChars");
 listFemalesButton.addEventListener("click", ()=> listFemaleChars());
@@ -35,25 +35,15 @@ function listFemaleChars(){getData().then(data => {
 // erzeugt neues Array nur mit Namen der Charaktere
 function mapCharactersByAlias(allChars) {
     console.log(allChars.map(char => char.aliases))
+    // const html = allChars.map(char => `<p>Name: ${char.aliases}</p>`).join("");
+    // document.querySelector("renderResult").innerHTML(html);
 }
 function listNames(){getData().then(data => {
     mapCharactersByAlias(data)
 })};
 
-function renderMaleCharacters() {
-    const listOfResult = document.getElementById("renderResult");
-    let resultList = listMaleChars();
-    for (let result of Object.values(resultList)) {
-        let newListItem = document.createElement("var");
-        newListItem.innerHTML = result;
-        listOfResult.appendChild(newListItem);
-    }
-}
 
-
-
-
-// function renderToDom(targetElementId, createElementType, addCreateElementClass, innerHTMLString) {
+// function renderToDom(targetElementId, createElementType, innerHTMLString) {
 //     // creates an HTML element of selected type
 //     // should be a string.
 //     // example: "li" creates a <li> element
@@ -65,7 +55,7 @@ function renderMaleCharacters() {
 //     targetElement.appendChild(element);
 //     // add specified class to previously
 //     // created element
-//     targetElement.classList.add(addCreateElementClass);
+//     // targetElement.classList.add(addCreateElementClass);
 //     // sets inner HTML of previously created element
 //     // to provided string. This can be valid HTML, just
 //     // some data of type String or undefined
@@ -74,22 +64,36 @@ function renderMaleCharacters() {
 //     return element;
 // }
 //
-// // renderToDom("charGoTDb", "div" , "name", getData());
-//
-//     //fetch --> array als rückgabe
-// // fetchPeople().then((people) =>
-// //     people.map((person) => {
-// //         renderToDom(
-// //             "listOfPeople",
-// //             "div",
-// //             `${person.first_name}, ${person.last_name}`
-// //         );
-// //     })
-// // );
-//
-// // async function fetchPeople() {
-// //     const people = fetch("https://reqres.in/api/users")
-// //         .then((response) => response.json())
-// //         .then((data) => data.data);
-// //     return people;
-// // }
+// // fetch --> array als rückgabe
+// getData().then(charDb =>
+//     charDb.map(char => {
+//         renderToDom(
+//             "renderResult",
+//             "div",
+//             `${char.name}`
+//         );
+//     })
+// );
+
+const newTest = document.getElementById("renderResult");
+let newTestItem=document.createElement("li");
+newTestItem.innerHTML="Apple";
+newTest.appendChild(newTestItem);
+
+const newTestList = document.getElementById("renderResult");
+const fruitList = ["Orange", "Lemon", "Kiwi"];
+fruitList.forEach(fruit => {
+    let newFruitListItem=document.createElement("li");
+    newFruitListItem.innerHTML = fruit;
+    newTestList.appendChild(newFruitListItem);
+})
+
+
+const newTest2 = document.getElementById("test");
+let newTestItem3=document.createElement("var");
+newTestItem3.innerHTML= `<h2>TestAnfang</h2>
+<p>hallo Du da!</p>
+<p>wie gehts dir?</p>
+<h2>TesteEnde</h2>`;
+newTest2.appendChild(newTestItem3);
+
